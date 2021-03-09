1dcbde94ac030edf9364fa57f2de232756451a07
KillianFrappartDev <killianFrappartDev@Gmail.com>
2021-03-09 15:34:24 +0100

    Responsive

--- src/components/Subscribe/index.js
+++ src/components/Subscribe/index.js
@@ -5,10 +5,10 @@ import './styles.css';
 const Subscribe = () => {
   return (
     <div className="subscribe-container">
-      <h2 className="subscribe-title">Stay tuned</h2>
+      {/* <h2 className="subscribe-title">Stay tuned</h2> */}
       <div className="subscribe-box">
           <input className="subscribe-input" placeholder="Write your email here...." />
-          <button className="subscribe-send"><IoMdMail color="#E0E0E0" size="2.5rem"/></button>
+          <button className="subscribe-send"><IoMdMail color="#E0E0E0" size="2rem"/></button>
       </div>
     </div>
   );
